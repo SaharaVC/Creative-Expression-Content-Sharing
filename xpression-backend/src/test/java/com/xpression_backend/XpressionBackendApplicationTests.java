e01b76fc4bd46549adcbb01c50563aea0d0a0112
Trinity Banks <trinitynaomibanks@gmail.com>
2026-04-10 12:48:23 -0400

    Initial commit of Xpression backend structure

---
+++ xpression-backend/src/test/java/com/xpression_backend/XpressionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpression_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpressionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
