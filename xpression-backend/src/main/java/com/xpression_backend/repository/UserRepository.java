6400f608ec69aef08a3052ebbdaa958005a606e6
SaharaVC <ms.scalloway2006@gmail.com>
2026-04-30 07:07:19 -0400

    Add findByEmail method to UserRepository

--- xpression-backend/src/main/java/com/xpression_backend/repository/UserRepository.java
+++ xpression-backend/src/main/java/com/xpression_backend/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.xpression_backend.repository;
 import com.xpression_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
