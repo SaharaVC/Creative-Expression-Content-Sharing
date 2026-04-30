630207fa6b7966dddbf99ec6770ed8e54a836b5f
SaharaVC <ms.scalloway2006@gmail.com>
2026-04-30 07:08:11 -0400

    Add method to find comments by post ID

--- xpression-backend/src/main/java/com/xpression_backend/repository/CommentRepository.java
+++ xpression-backend/src/main/java/com/xpression_backend/repository/CommentRepository.java
@@ -3,7 +3,9 @@ package com.xpression_backend.repository;
 import com.xpression_backend.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(Long postId);
 }
