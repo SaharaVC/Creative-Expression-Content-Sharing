06afe0c40c7a4cf0a95d2686014bec08f330d6ff
SaharaVC <ms.scalloway2006@gmail.com>
2026-04-30 07:05:50 -0400

    Add methods to PostRepository for custom queries

--- xpression-backend/src/main/java/com/xpression_backend/repository/PostRepository.java
+++ xpression-backend/src/main/java/com/xpression_backend/repository/PostRepository.java
@@ -3,6 +3,10 @@ package com.xpression_backend.repository;
 import com.xpression_backend.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {}
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByOrderByDateDesc();
+    List<Post> findByMediaType(String mediaType);
+}
