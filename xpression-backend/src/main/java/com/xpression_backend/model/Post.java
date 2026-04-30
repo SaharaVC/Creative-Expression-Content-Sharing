ac800667c586239807bc45a7cd9f3d0e7a8b4aa4
SaharaVC <ms.scalloway2006@gmail.com>
2026-04-30 06:58:20 -0400

    Add validation for mediaType in Post model

--- xpression-backend/src/main/java/com/xpression_backend/model/Post.java
+++ xpression-backend/src/main/java/com/xpression_backend/model/Post.java
@@ -5,16 +5,19 @@ import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import jakarta.validation.constraints.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table (name = "posts")
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Media type is required")
     private String mediaType; // text, image, or music
     private String caption;
     private LocalDateTime date = LocalDateTime.now();
